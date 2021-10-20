42a0d83f1f63578afb8a19857a69c9cb88be7485
Sandeep Soni <sandeepsonisrs01@gmail.com>
2021-10-20 20:36:51 +0530

    header footer and page content created

--- LightUI/src/app/app-routing.module.ts
+++ LightUI/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginpageComponent } from './loginpage/loginpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginpageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
